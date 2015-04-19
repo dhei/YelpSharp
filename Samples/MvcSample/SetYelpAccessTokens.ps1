1ca30f8dd791b8b0545053b4bc7de1313d86a1d6
Di Hei <dihei@microsoft.com>
2015-04-19 11:17:00 -0700

    Setup Yelp API Keys in script

---
+++ Samples/MvcSample/SetYelpAccessTokens.ps1
@@ -0,0 +1,7 @@
+# Set Yelp API keys, including AccessToken, AccessTokenSecret, ConsumerKey and ConsumerSecret
+# Yelp API keys can be accessed by https://www.yelp.com/developers/manage_api_keys
+
+[Environment]::SetEnvironmentVariable("YELP_CONSUMER_KEY", "", "Machine")
+[Environment]::SetEnvironmentVariable("YELP_CONSUMER_SECRET", "", "Machine")
+[Environment]::SetEnvironmentVariable("YELP_ACCESS_TOKEN", "", "Machine")
+[Environment]::SetEnvironmentVariable("YELP_ACCESS_TOKEN_SECRET", "", "Machine")
